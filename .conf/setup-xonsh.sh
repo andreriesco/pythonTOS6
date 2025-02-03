42fd14bb14bb223b3f3e993182ae281225b999e3
Andre Riesco <andre.riesco@toradex.com>
2025-02-03 16:51:36 -0300

    lkklklçlk

--- .conf/setup-xonsh.sh
+++ .conf/setup-xonsh.sh
@@ -23,3 +23,14 @@ pipx inject xonsh GitPython
 pipx inject xonsh xontrib-powerline2
 pipx inject xonsh python-lsp-server
 pipx inject xonsh pylsp-rope
+
+# add xonsh to the path if not already present
+if ! grep -q "export PATH=\$PATH:\$HOME/.local/bin" ~/.bashrc; then
+    echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
+fi
+
+# also for .xonshrc itself
+if ! grep -q "\$HOME/.local/bin" ~/.xonshrc; then
+    touch ~/.xonshrc
+    echo "\$PATH.insert(0, '$HOME/.local/bin')" >> ~/.xonshrc
+fi
